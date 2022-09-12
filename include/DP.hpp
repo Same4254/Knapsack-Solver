7d62159f4514b555f6faf3767e0ff055e4e922c2
Sara Wrotniak <sara.wrotniak@uconn.edu>
2022-09-12 18:25:21 -0400

    DP stack and loop

--- include/DP.hpp
+++ include/DP.hpp
@@ -1,17 +1,8 @@
 #pragma once
 
-#include "Knapsack.hpp"
 #include <unordered_map>
+#include <stack>
 
 using namespace Knapsack;
 
-// Since the table uses 64-bit identifiers for subproblems, 
-// this assumes the capacity is at most 2^32 and the number of items is at most 2^32
-struct DPKnapsack {
-    const KnapsackInstance instance;
-    std::unordered_map<uint64_t, KnapsackResult> table;
-
-    DPKnapsack(const KnapsackInstance &instance) : instance(instance) {}
-    KnapsackResult DPAlg();
-    void DPSubProb(const int64_t remCapacity, const int64_t itemIndex);
-};
+KnapsackResult DPKnapsack(const KnapsackInstance &instance);
--- src/DP.cpp
+++ src/DP.cpp
@@ -3,65 +3,80 @@
 
 using namespace Knapsack;
 
-KnapsackResult DPKnapsack::DPAlg() {
+// Since the table uses 64-bit identifiers for subproblems, this assumes the capacity <= 2^32 and number of items is <= 2^32
+KnapsackResult DPKnapsack(const KnapsackInstance &instance) {
     
-    if (instance.items.size() == 0) { // In case there are no items
+    if (instance.items.size() == 0) // In case there are no items
         return KnapsackResult();
-    } else {
-        DPSubProb(instance.capacity, 0);
-        // std::string fullProbId = std::to_string(instance.capacity) + "," + std::to_string(0);
-        uint64_t fullProbId = (instance.capacity << 32);
-        return table[fullProbId];
-    }
-}
 
-void DPKnapsack::DPSubProb(const int64_t remCapacity, const int64_t itemIndex) {
+    std::unordered_map<uint64_t, KnapsackResult> table;
+    int64_t remCapacity = instance.capacity;
+    int64_t itemIndex = 0;    
+    uint64_t fullProbId = (remCapacity << 32) | itemIndex; // Identifier of full problem
+    uint64_t subProbId = fullProbId; // Identifier of subproblem
+    std::stack<uint64_t> subProbIdStack;
+    subProbIdStack.push(subProbId);
+    int64_t untakenRemCapacity, takenRemCapacity;
+    uint64_t untakenSubProbId, takenSubProbId, untakenProfit, takenProfit;
 
-    // std::string subProbId = std::to_string(remCapacity) + "," + std::to_string(itemIndex); // Identifier of this subproblem
-    uint64_t subProbId = (remCapacity << 32) | itemIndex; // Identifier of this subproblem
-
-    int64_t untakenRemCapacity = remCapacity, takenRemCapacity = remCapacity - instance.items[itemIndex].weight;
-    // std::string untakenSubProbId = std::to_string(untakenRemCapacity) + "," + std::to_string(itemIndex + 1);
-    // std::string takenSubProbId = std::to_string(takenRemCapacity) + "," + std::to_string(itemIndex + 1);
-    uint64_t untakenSubProbId = (untakenRemCapacity << 32) | (itemIndex + 1);
-    uint64_t takenSubProbId = (takenRemCapacity << 32) | (itemIndex + 1);
-    int64_t untakenProfit = 0, takenProfit = 0;
-    
-    // If we don't take item i
-    if (itemIndex < (instance.items.size() - 1)) { // If this is not the last item
-        if (table.count(untakenSubProbId) == 0) // If we don't yet have result for remainder
-            DPSubProb(untakenRemCapacity, itemIndex + 1);
-        // Find what would be profit for remainder if we don't take item i
-        for (const auto &item : table[untakenSubProbId].items)
-            untakenProfit += item.value;
-    }
-
-    // If we can take item i (it fits)
-    if (takenRemCapacity >= 0) {
+    while (subProbIdStack.size() > 0) {
+       
+        subProbId = subProbIdStack.top();
+        remCapacity = subProbId >> 32;
+        itemIndex = subProbId & (((uint64_t)1 << 32) - 1);
+        untakenRemCapacity = remCapacity;
+        takenRemCapacity = remCapacity - instance.items[itemIndex].weight;
+        untakenSubProbId = (untakenRemCapacity << 32) | (itemIndex + 1);
+        takenSubProbId = (takenRemCapacity << 32) | (itemIndex + 1);
+        untakenProfit = 0;
+        takenProfit = 0;
+        
+        // If we don't take item i
         if (itemIndex < (instance.items.size() - 1)) { // If this is not the last item
-            if (table.count(takenSubProbId) == 0) // If we don't yet have result for remainder
-                DPSubProb(takenRemCapacity, itemIndex + 1);
-            // Find what would be profit for remainder if we take item i
-            for (const auto &item : table[takenSubProbId].items)
-                takenProfit += item.value;
+            if (table.count(untakenSubProbId) == 0) { // If we don't yet have result for remainder
+                subProbIdStack.push(untakenSubProbId);
+                continue;
+            }
+            // Find what would be profit for remainder if we don't take item i
+            for (const auto &item : table[untakenSubProbId].items)
+                untakenProfit += item.value;
         }
-        // Don't forget we take item i too
-        takenProfit += instance.items[itemIndex].value;
+
+        // If we can take item i (it fits)
+        if (takenRemCapacity >= 0) {
+            if (itemIndex < (instance.items.size() - 1)) { // If this is not the last item
+                if (table.count(takenSubProbId) == 0) { // If we don't yet have result for remainder
+                    subProbIdStack.push(takenSubProbId);
+                    continue;
+                }
+                // Find what would be profit for remainder if we take item i
+                for (const auto &item : table[takenSubProbId].items)
+                    takenProfit += item.value;
+            }
+            // Don't forget we take item i too
+            takenProfit += instance.items[itemIndex].value;
+        }
+
+        // Pick best choice
+        if (itemIndex == (instance.items.size() - 1)) { // If this is the last item
+            table[subProbId] = KnapsackResult();
+            if (untakenProfit < takenProfit) // Take item i
+                table[subProbId].items.push_back(instance.items[itemIndex]);
+        } else { // If this is not the last item
+            if (untakenProfit >= takenProfit) { // Don't take item i
+                table[subProbId] = table[untakenSubProbId];
+            } else { // Take item i
+                table[subProbId] = table[takenSubProbId];
+                table[subProbId].items.push_back(instance.items[itemIndex]);
+            }
+        }
+
+        // Done here, go back up the stack
+        subProbIdStack.pop();
+
     }
 
-    // Pick best choice
-    if (itemIndex == (instance.items.size() - 1)) { // If this is the last item
-        table[subProbId] = KnapsackResult();
-        if (untakenProfit < takenProfit) // Take item i
-            table[subProbId].items.push_back(instance.items[itemIndex]);
-    } else { // If this is not the last item
-        if (untakenProfit >= takenProfit) { // Don't take item i
-            table[subProbId] = table[untakenSubProbId];
-        } else { // Take item i
-            table[subProbId] = table[takenSubProbId];
-            table[subProbId].items.push_back(instance.items[itemIndex]);
-        }
-    }
+    return table[fullProbId];
 
 }
 
